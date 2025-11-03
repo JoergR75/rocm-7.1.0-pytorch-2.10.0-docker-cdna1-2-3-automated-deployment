9ba897078098b72e7ceaf367420c595d21b605d4
Jörg Roskowetz <141847198+JoergR75@users.noreply.github.com>
2025-11-03 15:21:09 +0100

    Update script_module_ROCm_710_Ubuntu_22.04-24.04_pytorch_2.10.0_server.sh

--- script_module_ROCm_710_Ubuntu_22.04-24.04_pytorch_2.10.0_server.sh
+++ script_module_ROCm_710_Ubuntu_22.04-24.04_pytorch_2.10.0_server.sh
@@ -276,10 +276,35 @@ source ~/.bashrc
 # create test script
 cd ~
 cat <<EOF > test.py
+#!/usr/bin/env python3
+
 import torch
 import subprocess
+import re
+import os
 
-print("\nPyTorch version:", torch.__version__)
+def get_cpu_model():
+    with open("/proc/cpuinfo") as f:
+        for line in f:
+            if "model name" in line:
+                return line.split(":")[1].strip()
+
+print("\nInstalled CPU:", get_cpu_model())
+
+def get_total_memory_gb():
+    with open("/proc/meminfo") as f:
+        for line in f:
+            if line.startswith("MemTotal:"):
+                # Extract the numeric value in kB
+                mem_kb = int(re.findall(r'\d+', line)[0])
+                # Convert to GB (1 GB = 1024^2 kB)
+                mem_gb = mem_kb / (1024 ** 2)
+                return f"Total System-Memory: {mem_gb:.0f} GB"
+
+if __name__ == "__main__":
+    print(get_total_memory_gb())
+
+print("PyTorch version:", torch.__version__)
 print("ROCm version:", subprocess.getoutput("/opt/rocm/bin/hipconfig --version"))
 print("Is ROCm available:", torch.version.hip is not None)
 print("Number of GPUs:", torch.cuda.device_count())
